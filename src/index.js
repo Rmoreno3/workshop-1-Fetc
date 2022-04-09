d13dc228945f48a907f71e280de075b3930e3527
Rmoreno3 <reynaldomoreno173@gmail.com>
2022-04-08 22:10:17 -0400

    Descargando info de la API y creando los nodos en el HTML

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+// API
+const URL = 'https://platzi-avo.vercel.app/api/avo'
 
-console.log('Happy hacking :)')
+//Web API usando ASYNC / AWAIT
+//Conectarnos al server
+//Procesar la respuesta, y convertirla en JSON
+//JSON -> Data -> Renderizar informacion
+
+const fetchData = async () => {
+  const response = await fetch(URL);
+  const data = await response.json();
+  const allItems = [];
+
+  data.data.forEach((item) => {
+    //Create Title
+    const title =
+    document.createElement('h1');
+    //Create Image
+    const image =
+    document.createElement('img');
+    //Create Price
+    const price =
+    document.createElement('span');
+
+    const container = document.createElement('div');
+    container.append(title, image, price);
+
+    allItems.push(container)
+
+  })
+  
+  document.body.append(...allItems)
+}
+
+fetchData();
